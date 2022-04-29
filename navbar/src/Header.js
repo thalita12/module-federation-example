c21c65074b107e530720908224b60b372d4e2f70
Thalita <thalita@cora.com.br>
2022-04-29 17:07:45 -0300

    fix: fix logic tabs - No próximo encontro adicionar react-router-dom e melhorar a lógica de troca de abas

--- navbar/src/Header.js
+++ navbar/src/Header.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
-const Header = () => (
-    <header>
-        <ul>
-            <li><a className="active react" href="/react-app">React</a></li>
-            <li><a className="vue" href="/vue-app">Vue</a></li>
-            <li><a className="angular" href="/angular-app">Angular</a></li>
-        </ul>
-    </header>
-)
+const Header = () => {
+    const [activeItem, setActiveItem] = useState('react')
+
+    const handleClick = (id) => {
+        setActiveItem(id)
+    }
+
+    return (
+        <header>
+            <ul>
+                <li><a className={activeItem === 'react' ? 'active react' : '' } href="/react-app" onClick={() => handleClick('react')}>React</a></li>
+                <li><a className={activeItem === 'vue' ? 'active vue' : '' } href="/vue-app" onClick={() => handleClick('vue')}>Vue</a></li>
+                <li><a className={activeItem === 'angular' ? 'active angular' : '' } href="/angular-app" onClick={() => handleClick('angular')}>Angular</a></li>
+            </ul>
+        </header>
+    )
+}
 
 export default Header
